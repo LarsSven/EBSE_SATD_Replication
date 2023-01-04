26068554bf7d76808431eefb7df2b904d3d62b39
Lars Andringa <l.s.andringa@rug.nl>
2023-01-04 21:47:25 +0100

    Fixed wrong sample

--- readdata.py
+++ readdata.py
@@ -1,6 +1,6 @@
 import csv, os
 
-with open('sampled.csv', 'r', newline='') as csvfile:
+with open('sampled_German.csv', 'r', newline='') as csvfile:
     reader = csv.reader(csvfile)
     header = next(reader)
 
@@ -9,12 +9,13 @@ with open('sampled.csv', 'r', newline='') as csvfile:
         next(reader)
     
     for row in reader:
-        os.system('clear')
-        print("Entry:", skip+1)
-        print("Project:", row[0], "PR:", row[1], "URL:", f'https://github.com/apache/{row[0]}/pull/{row[1]}')
-        print("Pull_type:", row[2])
-        print("Classification:", row[5])
-        print("Indicator:", row[6])
-        print("Text:", row[4])
-        input("Press Enter to continue...")
+        #os.system('clear')
+        #print("Entry:", skip+1)
+        #print("Project:", row[0], "PR:", row[1], "URL:", f'https://github.com/apache/{row[0]}/pull/{row[1]}')
+        #print("Pull_type:", row[2])
+        #print("Classification:", row[5])
+        #print("Indicator:", row[6])
+        #print("Text:", row[4])
+        #input("Press Enter to continue...")
+        print(row[0])
         skip += 1
