0a47e88a5f356aff9a2035f561616ceab354b914
Koen Bolhuis <koen.bolhuis@gmail.com>
2023-01-08 10:44:49 +0100

    Update code changes script to download archives

--- fetch_diffs.py
+++ fetch_diffs.py
@@ -1,4 +1,5 @@
-import csv, collections, os, os.path, sys, shutil, requests, json, time
+import csv, collections, os, os.path, sys, requests, json
+from multiprocessing import Pool
 
 
 IN_FILE = 'Preprocessing/new.csv'
@@ -26,10 +27,19 @@ with open(IN_FILE, 'r') as csvfile:
 
 n_pulls = len(repo_pull_items)
 
-print('Fetching pull request data')
 
-for i, ((repo, pull_number), items) in enumerate(repo_pull_items.items()):
-    print(f'\rProcessing {i+1}/{n_pulls}', end='', flush=True)
+def download_archive(repo, sha, path):
+    response = requests.get(f'https://github.com/apache/{repo}/archive/{sha}.tar.gz', headers=HEADERS)
+    response.raise_for_status()
+
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
+def process_pr(args):
+    (repo, pull_number), items = args
+
+    print(f'Processing {repo}#{pull_number}')
 
     pull_path = f'{OUT_DIR}/{repo}/{pull_number}'
 
@@ -52,13 +62,25 @@ for i, ((repo, pull_number), items) in enumerate(repo_pull_items.items()):
         with open(f'{pull_path}/data.json', 'r') as pull_data_file:
             pull_data = json.load(pull_data_file)
 
-    # Fetch diffs if they don't exist
+    # Fetch diffs and archives if the item is a summary/description
 
-    if not os.path.exists(f'{pull_path}/changes.diff'):
-        diff_response = requests.get(pull_data['diff_url'], headers=HEADERS)
-        diff_response.raise_for_status()
+    is_summary_or_description = any(item['pull_type'] in ('summary', 'description') for item in items)
 
-        with open(f'{pull_path}/changes.diff', 'w') as diff_data_file:
-            diff_data_file.write(diff_response.text)
+    if is_summary_or_description:
+        if not os.path.exists(f'{pull_path}/changes.diff'):
+            diff_response = requests.get(pull_data['diff_url'], headers=HEADERS)
+            diff_response.raise_for_status()
 
-print('\nDone')
+            with open(f'{pull_path}/changes.diff', 'w') as diff_data_file:
+                diff_data_file.write(diff_response.text)
+
+        if not os.path.exists(f'{pull_path}/before.tar.gz') or not os.path.exists(f'{pull_path}/after.tar.gz'):
+            download_archive(repo, pull_data['base']['sha'], f'{pull_path}/before.tar.gz')
+            download_archive(repo, pull_data['head']['sha'], f'{pull_path}/after.tar.gz')
+
+
+with Pool(8) as p:
+    p.map(process_pr, repo_pull_items.items())
+
+
+print('Done')
