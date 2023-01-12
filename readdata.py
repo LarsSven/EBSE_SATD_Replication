4952fb21a91c0efe15e69a5f513295ae57fb6ab2
Lars Andringa <l.s.andringa@rug.nl>
2023-01-12 16:10:03 +0100

    Improved URL in reading script

--- readdata.py
+++ readdata.py
@@ -11,7 +11,11 @@ with open('sampled.csv', 'r', newline='') as csvfile:
     for row in reader:
         os.system('clear')
         print("Entry:", skip+1)
-        print("Project:", row[0], "PR:", row[1], "URL:", f'https://github.com/apache/{row[0]}/pull/{row[1]}')
+        print("Project:", row[0], "PR:", row[1])
+        if row[2] == "review":
+            print("URL:", f'https://github.com/apache/{row[0]}/pull/{row[1]}#discussion_r{row[3]}')
+        else:
+            print("URL:", f'https://github.com/apache/{row[0]}/pull/{row[1]}#issuecomment-{row[3]}')
         print("Pull_type:", row[2])
         print("Classification:", row[5])
         print("Indicator:", row[6])
