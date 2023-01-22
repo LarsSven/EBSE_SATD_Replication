3d91f2f5c017c0e7cef7fc94af52b3f1f7c52620
Koen Bolhuis <koen.bolhuis@gmail.com>
2023-01-22 17:18:28 +0100

    Calculate kappa score

---
+++ kappa.py
@@ -0,0 +1,26 @@
+import csv
+from sklearn.metrics import cohen_kappa_score
+
+
+def print_kappa(filename):
+    with open(filename, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        rows = list(reader)
+        names = reader.fieldnames
+
+        data = {
+            name: [row[name] for row in rows]
+            for name in names
+        }
+
+        for i in range(len(names)):
+            for j in range(i + 1, len(names)):
+                n1 = names[i]
+                n2 = names[j]
+                kappa = cohen_kappa_score(data[n1], data[n2])
+                print(f'{n1} - {n2}: {kappa:.3f}')
+
+
+for file in ['round1.csv', 'round2.csv', 'round4.csv']:
+    print(file)
+    print_kappa(f'Agreement/{file}')
